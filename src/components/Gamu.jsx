17bc9ccb60baac68ede75f7ad76fc2328be4153c
Erzenkell <ferrier.sammy@gmail.com>
2023-12-13 23:21:54 +0100

    key control

--- src/components/Gamu.jsx
+++ src/components/Gamu.jsx
@@ -1,14 +1,59 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {generateMap} from "../utils/generateMap";
 import { loadAssets } from "../utils/loadAssets";
+//import { keyDownHandler } from "../utils/keyDownHandler";
 
-const Gamu = () => {
+const Gamu = (props) => {
 
+    //canvas
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+    }, [])
+
+    //map
     const map = generateMap();
-    loadAssets();
+
+    //assets
+    const [assetsLoaded, setAssetsLoaded] = useState(false);
+    const [assets, setAssets] = useState(null);
+
+    loadAssets().then((assets) => {
+        setAssetsLoaded(true);
+        setAssets(assets);
+    });
+
+    //keyPress
+    const [pressedKeys, setPressedKeys] = useState(new Set());
+    
+    useEffect(() => {
+        const getKey = (event) => {
+            setPressedKeys(new Set());
+            if (event.defaultPrevented) {
+                return; // Do nothing if the event was already processed
+            }
+            const key = event.key;
+            if(!pressedKeys.has(key)) {
+                setPressedKeys((prev) => {
+                    return new Set(prev.add(key));
+                });
+            }
+            event.preventDefault();
+        }
+        document.addEventListener('keydown', getKey, true);
+        return () => {
+            document.removeEventListener('keydown', getKey, true);
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log(pressedKeys);
+    }, [pressedKeys]);
 
     return(
-        <h2>oui</h2>
+        <canvas ref={canvasRef} {...props}/>
     )
 }
 
--- src/utils/loadAssets.js
+++ src/utils/loadAssets.js
@@ -1,4 +1,32 @@
+const importImages = async (type, path) => {
+    const charResponse = await fetch(`${path}/${type}/${type}.json`);
+    const charFileNames = await charResponse.json();
+
+    const charImagePromises = charFileNames.map(async (fileName) => {
+        const imageUrl = `${path}/${type}/${fileName}`;
+        const image = new Image();
+
+        // Create a promise that resolves when the image is loaded
+        const imageLoadedPromise = new Promise((resolve) => {
+            image.onload = () => resolve(image);
+        });
+
+        // Set the image source to start loading
+        image.src = imageUrl;
+
+        // Wait for the image to load
+        await imageLoadedPromise;
+
+        return image;
+    });
+
+    return Promise.all(charImagePromises);
+}
+
 export const loadAssets = async () => {
-    const charSprite = require.context('../assets/characters', true);
-    console.log(charSprite.keys());
+    const path = '/src/assets/sprite';
+    const Assets = {};
+    const characterImages = await importImages('character', path);
+    Assets.character = characterImages;
+    return Assets;
 }
